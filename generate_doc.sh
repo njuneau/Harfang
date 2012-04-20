aa8fe9db58d46bab7cf29020e45ebb1100781e2e
Nicolas Juneau <n.juneau@gmail.com>
2012-04-20 02:01:14 -0400

    Added release information and fixed doc generation

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find -type f -name '*.hx' | sed -e '/^\.\/src\/\(haxe\|server\)/d;s/\.\/src\///;' | xargs haxe -php out -cp src -xml haxedoc.xml
+find -type f -name '*.hx' | sed -e '/^\.\/src\/\(haxe\|server\|harfang\/tests\)/d;s/\.\/src\///;' | xargs haxe -php out -cp src -xml haxedoc.xml
